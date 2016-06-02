df918fd514e069cc9f7d2480f78093723c66c62f
amit <amit.bracha@mail.huji.ac.il>
2016-06-02 15:22:06 +0300

    updated FunctionParameter class

--- src/oop/ex6/FunctionParameter.java
+++ src/oop/ex6/FunctionParameter.java
@@ -8,7 +8,13 @@ import java.util.LinkedList;
 public class FunctionParameter {
     private String Name;
     private LinkedList<Var> varList;
-    
+    private String functionList;
+
+    FunctionParameter(String name, LinkedList<Var> varList, String functionList){
+        this.Name = name;
+        this.varList = varList;
+        this.functionList = functionList;
+    }
 
 
 }
