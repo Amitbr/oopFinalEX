45b5437705f152df10358a93f5b030f4fb7834b2
amit <amit.bracha@mail.huji.ac.il>
2016-06-02 14:32:55 +0300

    created FunctionParameter class

---
+++ src/oop/ex6/FunctionParameter.java
@@ -0,0 +1,14 @@
+package oop.ex6;
+
+import java.util.LinkedList;
+
+/**
+ * Created by amit on 02/06/2016.
+ */
+public class FunctionParameter {
+    private String Name;
+    private LinkedList<Var> varList;
+    
+
+
+}
--- src/oop/ex6/varsFactory.java
+++ src/oop/ex6/varsFactory.java
@@ -1,69 +1,89 @@
 package oop.ex6;
+
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 /**
  * Created by amit on 01/06/2016.
  */
-public class varsFactory {
-    private final Pattern LEGAL_LINE = Pattern.compile( " *\\w+ *.*;");
-    private final Pattern LEGAL_VAR_NAME = Pattern.compile("([_]{1}[\\w]+)|([a-zA-Z]+[\\w]*)");
-    private final Pattern LEGAL_ASSIGNMENT_SYNTAX = Pattern.compile("^= *");
-    private final Pattern LEGAL_INT_VALUES = Pattern.compile(" *\\d+ *$");
-    private final Pattern STARTING_SPACES = Pattern.compile("^ *");
+public class VarsFactory {
+    private final static Pattern LEGAL_LINE = Pattern.compile(" *\\w+ *.*;");
+    private final static Pattern LEGAL_VAR_NAME = Pattern.compile("([_]{1}[\\w]+)|([a-zA-Z]+[\\w]*)");
+    private final static Pattern LEGAL_ASSIGNMENT_SYNTAX = Pattern.compile("^= *+");
+    private final static Pattern LEGAL_INT_VALUES = Pattern.compile(" *+-?\\d+ *+$");
+    private final static Pattern STARTING_SPACES = Pattern.compile("^ *+");
+    private final static Pattern FIRST_WORLD = Pattern.compile("\\w++ ");
 
 
-//    enum SavedWord {
-//        FINAL("final"), INT("int"), STRING("String"), CHAR("char"), DOUBLE("double"), BOOLEAN("boolean"),
-//        VOID("void");
-//
-//        private final String VALUE;
-//
-//        SavedWord(String value) {
-//            this.VALUE = value;
-//        }
-//
-//        String getVALUE(){
-//            return this.VALUE;
-//        }
-//
-//    }
+    enum SavedWord {
+        FINAL("final", ""), INT("int", " *-?\\d+ *$"), STRING("String", " *\".*?\" *$"),
+        CHAR("char", " *\'.?\' *$"), DOUBLE("double", " *-?\\d+\\.\\d+|-?\\d+ *$"),
+        BOOLEAN("boolean", "false|true"), VOID("void", "");
 
-     LinkedList<vars> createVars(String line) throws SyntaxException {
-         Matcher islegalLineMatcher = LEGAL_LINE.matcher(line);
-         if(!islegalLineMatcher.matches() || (line.indexOf(';')!=line.lastIndexOf(";"))){
-             throw new SyntaxException();
-         }
+        private final String VALUE;
+        private final Pattern LEGAL_ASSIGNMENT_VALUE;
+
+        SavedWord(String value, String assignment_value_regex) {
+            this.VALUE = value;
+            this.LEGAL_ASSIGNMENT_VALUE = Pattern.compile(assignment_value_regex);
+        }
+
+        String getVALUE() {
+            return this.VALUE;
+        }
+
+        Pattern getPattern() {
+            return LEGAL_ASSIGNMENT_VALUE;
+        }
+
+    }
+
+    static LinkedList<Var> createVars(String line) throws SyntaxException {
+        Matcher isLegalLineMatcher = LEGAL_LINE.matcher(line);
+        if (!isLegalLineMatcher.matches() || (line.indexOf(';') != line.lastIndexOf(";"))) {
+            throw new SyntaxException();
+        }
         //notice that a line that starts with space will pass the test
-        Pattern firstWordRegex = Pattern.compile("\\w++ ");
-        Matcher firstWordMatcher = firstWordRegex.matcher(line);
-        if (firstWordMatcher.find()){
-            String firstWord = line.substring(firstWordMatcher.start(),firstWordMatcher.end());
+        Matcher firstWordMatcher = FIRST_WORLD.matcher(line);
+        if (firstWordMatcher.find()) {
+            String firstWord = line.substring(firstWordMatcher.start(), firstWordMatcher.end());
             String restOfTheLine = line.substring(firstWordMatcher.end());
-            LinkedList<vars> newVars = new LinkedList<>();
-            restOfTheLine = restOfTheLine.substring(0,restOfTheLine.lastIndexOf(";")); //removing ';'
+            LinkedList<Var> newVars = new LinkedList<>();
+            restOfTheLine = restOfTheLine.substring(0, restOfTheLine.lastIndexOf(";")); //removing ';'
             String[] varsValues = restOfTheLine.split(",");
-            switch (firstWord){
+            switch (firstWord) {
                 case "final":
-                    LinkedList<vars> finalVars = createVars(restOfTheLine);
-                    //add here to change var to FINAL
+                    LinkedList<Var> finalVars = createVars(restOfTheLine);
+                    for (Var var : finalVars) {
+                        var.changeToFinal();
+                    }
                     return finalVars;
                 case "int":
-                    for (String value: varsValues) {
-                        newVars.add(createInt(value));
+                    for (String value : varsValues) {
+                        newVars.add(createVar(value, SavedWord.INT));
                     }
                     return newVars;
                 case "String":
-                    for (String value: varsValues) {
-                        newVars.add(createString(value));
+                    for (String value : varsValues) {
+                        newVars.add(createVar(value, SavedWord.STRING));
                     }
                     return newVars;
                 case "char":
-                    break;
+                    for (String value : varsValues) {
+                        newVars.add(createVar(value, SavedWord.CHAR));
+                    }
+                    return newVars;
                 case "double":
-                    break;
+                    for (String value : varsValues) {
+                        newVars.add(createVar(value, SavedWord.DOUBLE));
+                    }
+                    return newVars;
                 case "boolean":
-                    break;
+                    for (String value : varsValues) {
+                        newVars.add(createVar(value, SavedWord.BOOLEAN));
+                    }
+                    return newVars;
                 default:
                     throw new SyntaxException();
 
@@ -71,41 +91,39 @@ public class varsFactory {
         } else {
             throw new SyntaxException();
         }
-         return null;
     }
 
-    vars createInt (String value) throws SyntaxException{
+    static Var createVar(String value, SavedWord savedWord) throws SyntaxException {
         Matcher removeStartingSpacesMatcher = STARTING_SPACES.matcher(value);
         value = value.substring(removeStartingSpacesMatcher.end());
-        Matcher nameMacher = LEGAL_VAR_NAME.matcher(value);
-        if(!nameMacher.find()){
+        Matcher nameMatcher = LEGAL_VAR_NAME.matcher(value);
+        if (!nameMatcher.find()) {
             throw new SyntaxException();
         }
-        String name = value.substring(nameMacher.start(),nameMacher.end());
+        String name = value.substring(nameMatcher.start(), nameMatcher.end());
         boolean isAssignedWithValue = false;
-        String restOfTheValue = value.substring(nameMacher.end());
+        String restOfTheValue = value.substring(nameMatcher.end());
         removeStartingSpacesMatcher = STARTING_SPACES.matcher(restOfTheValue);
         restOfTheValue = restOfTheValue.substring(removeStartingSpacesMatcher.end());
-        if(restOfTheValue.equals("")){
-
-        }else {
+        if (!restOfTheValue.equals("")) {
             Matcher checkForAssignmentMatcher = LEGAL_ASSIGNMENT_SYNTAX.matcher(restOfTheValue);
-            if(!checkForAssignmentMatcher.find()){
+            if (!checkForAssignmentMatcher.find()) {
                 throw new SyntaxException();
-            }else {
+            } else {
                 restOfTheValue = restOfTheValue.substring(checkForAssignmentMatcher.end());
-                Matcher checkAssignmentValueMatcher = LEGAL_INT_VALUES.matcher(restOfTheValue);
-                if(!checkAssignmentValueMatcher.matches()){
+                Matcher checkAssignmentValueMatcher = savedWord.getPattern().matcher(restOfTheValue);
+                if (!checkAssignmentValueMatcher.matches()) {
                     throw new SyntaxException();
                 }
                 isAssignedWithValue = true;
             }
         }
 
-        return null;
+        return new Var(name, savedWord.getVALUE(), isAssignedWithValue, false);
     }
 
-    vars createString (String values){
-        return null;
+    static void updateVar(String line){
+
     }
+
 }
